0478f03507ab430fabb59476b90ec8399a8ba700
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2023-11-16 15:16:50 -0800

    Add __repr__ method

--- multicast.py
+++ multicast.py
@@ -27,6 +27,9 @@ class Switch:
     def __str__(self):
         return self.name
 
+    def __repr__(self):
+        return self.name
+
 
 class Router(Switch):  # A Router is a subclass of a Switch
     def __init__(self, name, parent_router, trust_domain):
@@ -69,19 +72,19 @@ def main():
     routerRoot = Router("A", None, None)
 
     # Create trust domain A with one router and two switches
-    domainA = TrustDomain("A")
-    routerA = Router("A", routerRoot, domainA)
-    switch1 = Switch("1", domainA)
+    domainA = TrustDomain("domainA")
+    routerA = Router("routerA", routerRoot, domainA)
+    switch1 = Switch("switch1", domainA)
     switch1.neighbors.add(routerA)
-    switch2 = Switch("2", domainA)
+    switch2 = Switch("switch2", domainA)
     switch2.neighbors.add(routerA)
 
     # Create trust domain B with one router and two switches
     domainB = TrustDomain("B")
-    routerB = Router("B", routerRoot, domainB)
-    switch3 = Switch("3", domainB)
+    routerB = Router("routerB", routerRoot, domainB)
+    switch3 = Switch("switch3", domainB)
     switch3.neighbors.add(routerB)
-    switch4 = Switch("4", domainB)
+    switch4 = Switch("switch4", domainB)
     switch4.neighbors.add(routerB)
 
     # Create multicast tree in domain A
